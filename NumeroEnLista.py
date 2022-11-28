76727f96e72b88cf8537fa6641d06935bdcfec58
Miguel Segovia Gil <41863090+msegoviag@users.noreply.github.com>
2022-11-28 12:48:43 +0100

    Minor fix: better feedback.

--- NumeroEnLista.py
+++ NumeroEnLista.py
@@ -3,9 +3,9 @@
 def estaEnLista(num, lista):
 
     if num in lista:
-        print("El número está dentro de la lista.")
+        print("El número " + str(num) + " está dentro de la lista.")
     else:
-        print("El número no está dentro de la lista.")
+        print("El número  " + str(num) + "  no está dentro de la lista.")
 
 # Se comprueba si el número está dentro del rango, si lo está entonces se chequea si está dentro de la lista.
 
@@ -13,17 +13,17 @@ def estaEnLista(num, lista):
 def estaEnRango(valor, minimo, maximo):
 
     if valor < minimo or valor > maximo:
-        print("El número está fuera del rango de la lista")
+        print("El número " + str(valor) + " está fuera del rango de la lista")
         return
     else:
-        print("El número esta dentro del rango de la lista")
+        print("El número " + str(valor) + " está dentro del rango de la lista")
 
 # Ejecución segura ante fallos, solo se permitirá numeros no letras.
 
 
 try:
     lista = [6, 14, 11, 3, 2, 1, 15, 19]
-    estaEnRango(3, 5, 10)
-    estaEnLista(3, lista)
+    estaEnRango(5, 5, 10)
+    estaEnLista(-1, lista)
 except (ValueError, TypeError):
     print("Introduce únicamente números")
